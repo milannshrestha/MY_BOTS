580560b3c2590beb1b37e3fcbf52b4d0edf825cd
Milann SHRESTHA <31887847+milannshrestha@users.noreply.github.com>
2019-12-17 19:31:37 +0545

    Create refined-dfh.py
    this bot gets actiavted when the servers disk usafe is greater than 70%

---
+++ refined-dfh.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+import os
+import time
+
+import shutil
+import datetime
+
+bot = commands.Bot(command_prefix="dontchangethisxxx1x")
+client = discord.Client()
+'''
+def linePrint():
+	lines = open('dfh.txt').read()
+	return lines
+'''
+
+
+@client.event
+async def on_ready():
+	total, used, free = shutil.disk_usage('/')
+	print('[*] Bot Script is Logged in now ')
+	used1 = (used /(2**30))
+	total1 = round((total/(2**30)), 2)
+	per = ((used1/total1)*100)
+	a = round(per, 2)
+	print ('percent {}%'.format(a))
+
+	x = 69
+#	while x < 70:
+	if per < 70:
+		now = datetime.datetime.now()
+		heredate =  ("[+] Alert pushed at:" + now.strftime("%Y-%m-%d %H:%M:%S"))
+		#print (heredate)
+		channel = client.get_channel(646700252690251808)
+		await channel.send("```"+ heredate+'\n'+ 'Disk Storage Used Maximum.\nDisk Usage: {}%\nTotal Disk Size: {}GB'.format(a, total1)+"```")
+		print (heredate)
+#		time.sleep(13)
+
+
+
+client.run('NjQ2NDExMzMwODA2NDgwOTEw.XdU5CQ.fuOSaVgGfEyR4o-V991fwE7byro')
+
+
